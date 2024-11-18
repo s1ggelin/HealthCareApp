af54fa3ea3cf575c3f6c5d492347746777afa7dd
Andreas Siggelin <andreas.siggelin@learnet.se>
2024-11-18 11:13:49 +0100

    initial but not complete

--- Pages/Dashboard/Admin/AdminDashboard.cshtml.cs
+++ Pages/Dashboard/Admin/AdminDashboard.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using HealthCareABApi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,11 +13,44 @@ namespace HealthCareWebb.Pages.Dashboard
     [Authorize(Policy = "AdminOnly")]
     public class AdminDashboardModel : PageModel
     {
+        private readonly HttpClient _httpClient;
+
+        public AdminDashboardModel(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
         public string Username { get; private set; }
+
+        [BindProperty]
+        public int CaregiverId { get; set; }
+
+        [BindProperty]
+        public List<string> SelectedTimeSlots { get; set; } = new List<string>();
 
         public void OnGet()
         {
             Username = User.Identity.Name;
+        }
+
+        public IActionResult OnPostAddAvailability()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Process the selected time slots
+            foreach (var timeSlot in SelectedTimeSlots)
+            {
+                if (DateTime.TryParse(timeSlot, out DateTime slotDateTime))
+                {
+                  
+                }
+            }
+
+            // Save changes and redirect
+            return RedirectToPage();
         }
     }
 }
